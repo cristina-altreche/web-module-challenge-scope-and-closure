550f0a41efe590bfa26c3f657d9a340ac9d21807
Cristina Altreche <c.altreche@gmail.com>
2020-05-16 13:16:56 -0500

    Task 2 complete

--- index.js
+++ index.js
@@ -57,9 +57,11 @@ console.log(counter2());
 
 Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/) {
-  /*Code Here*/
+function inning() {
+  return Math.floor(Math.random() * 3);
 }
+
+console.log(inning());
 
 /* Task 3: finalScore()
 
